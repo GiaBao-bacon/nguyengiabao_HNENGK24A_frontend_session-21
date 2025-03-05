325f69a21c7f1fec3949a24cc51ae416dd35f4e5
GiaBao-bacon <nguyengiabao102006bacon@gmail.com>
2025-03-05 22:56:03 +0700

    upload bài 3

---
+++ jencont.js
@@ -0,0 +1,9 @@
+let sun;
+let passwork="2345678";
+let bacon;
+bacon=prompt("mời bạn nhập mật khẩu vào đây:")
+if(bacon===passwork){
+alert("chuẩn rồi good choop");
+}else{
+alert("sai rồi baby ơi");
+}
