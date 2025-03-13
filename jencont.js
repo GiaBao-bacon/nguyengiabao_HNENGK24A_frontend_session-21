066f5701a2817b79566b04cbb33ddd29567ad574
GiaBao-bacon <nguyengiabao102006bacon@gmail.com>
2025-03-13 11:50:28 +0700

    Delete jencont.js

--- jencont.js
+++
@@ -1,9 +0,0 @@
-let sun;
-let passwork="2345678";
-let bacon;
-bacon=prompt("mời bạn nhập mật khẩu vào đây:")
-if(bacon===passwork){
-alert("chuẩn rồi good choop");
-}else{
-alert("sai rồi baby ơi");
-}
